e1c97a5c68e2b7adf5b55139a25bfce8dd22a341
lenovo <629848089@qq.com>
2025-04-03 16:43:45 +0800

    调整腾讯混元api和百度文心一言api

--- app/utils/renellm/llm_completion_utils.py
+++ app/utils/renellm/llm_completion_utils.py
@@ -1,7 +1,8 @@
 import os
+import time
+import requests
 
 from openai import OpenAI
-import time
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 
 # for gpt
@@ -10,7 +11,6 @@ def get_llm_responses(model, messages, temperature, retry_times):
     qwen_api_key = os.environ.get("QWEN_API_KEY")
     deepseek_api_key = os.environ.get("DEEPSEEK_API_KEY")
     tencent_api_key = os.environ.get("TENCENT_API_KEY")
-    baidu_api_key = os.environ.get("BAIDU_API_KEY")
 
     # 默认调用
     client = OpenAI(
@@ -18,26 +18,34 @@ def get_llm_responses(model, messages, temperature, retry_times):
         base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
     )
 
+    # 阿里 通义千问 api
     if model == "qwen-plus":
         client = OpenAI(
             api_key=qwen_api_key,
             base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
         )
+    # deepseek 深度求索 api
     elif model == "deepseek-reasoner":
         client = OpenAI(
             api_key=deepseek_api_key,
             base_url="https://api.deepseek.com"
         )
+    # 腾讯 混元 api
     elif model == "hunyuan-turbos-latest":
         client = OpenAI(
             api_key=tencent_api_key,
             base_url="https://api.hunyuan.cloud.tencent.com/v1"
         )
+    # # 科大讯飞 星火 api
+    # elif model == "lite" :
+    #     client = OpenAI(
+    #         api_key=spark_api_key,
+    #         base_url="https://spark-api-open.xf-yun.com/v1/chat/completions"
+    #     )
+    # 百度 文心一言 api
     elif model == "ernie-4.5-8k-preview":
-        client = OpenAI(
-            api_key=baidu_api_key,
-            base_url="https://qianfan.bj.baidubce.com/v2/chat/completions"
-        )
+        response = get_baidu_response(model, messages)
+        return response
 
 
     try:
@@ -66,6 +74,38 @@ def get_llm_responses(model, messages, temperature, retry_times):
     time.sleep(1)
 
     return model_output
+
+# for baidu
+
+def get_baidu_access_token(api_key, secret_key):
+    url = "https://aip.baidubce.com/oauth/2.0/token"
+    params = {
+        "grant_type": "client_credentials",
+        "client_id": api_key,
+        "client_secret": secret_key
+    }
+    response = requests.post(url, params=params).json()
+    return response["access_token"]
+
+def get_baidu_response(attack_model,prompt):
+
+    BAIDU_API_KEY = os.environ.get("BAIDU_API_KEY")
+    BAIDU_SECRET_KEY = os.environ.get("BAIDU_SECRET_KEY")
+    access_token = get_baidu_access_token(BAIDU_API_KEY, BAIDU_SECRET_KEY)
+
+    url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+    params = {"access_token": access_token}
+    payload = {
+        "model": attack_model,
+        "messages": prompt,
+        "temperature": 0.5
+    }
+    response = requests.post(url, params=params, headers=headers, json=payload).json()
+    return response["result"]
 
 # for claude
 def claudeCompletion(model, max_tokens, temperature, prompt, retry_times, round_sleep, fail_sleep, api_key, base_url=None):
